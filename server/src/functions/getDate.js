11cdd3f973468834d06d603d8d2895e2d4e0e06e
Bryan Vera <bryan.ve.bv@gmail.com>
2020-04-25 21:16:22 -0500

    [ UPDATE ]

--- server/src/functions/getDate.js
+++ server/src/functions/getDate.js
@@ -1,14 +1,13 @@
-const getDate = (dateFormat) => {
-  const fullDate = dateFormat.toISOString().split("T");
-  const date = fullDate[0].split("-");
-  const time = fullDate[1].split(".")[0].split(":");
-  const year = date[0];
-  const month = date[1];
-  const day = date[2];
-  const hour = time[0] - 5;
-  const minutes = time[1];
 
-  return `${day}/${month}/${year} ${hour}:${minutes}`;
+const padStart = (number) => {
+  return number.toString().padStart(2, '0')
+}
+
+const getDate = (dt) => {
+  const date = [padStart(dt.getDate()), padStart(dt.getMonth() + 1), dt.getFullYear()].join('/')
+  const time = [padStart(dt.getHours()), padStart(dt.getMinutes())].join(':')
+  const fullDate = [date, time].join(' ')
+  return fullDate;
 };
 
 module.exports = getDate;
