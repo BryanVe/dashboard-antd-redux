b8c2c1bf9569b9008d6841fb6ac91fcbaea7d7ab
Bryan Vera <bryan.ve.bv@gmail.com>
2020-04-25 19:40:44 -0500

    [ UPDATE ]

--- server/src/functions/verifyToken.js
+++ server/src/functions/verifyToken.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 
 const verifyToken = (req, res, next) => {
   const token = req.headers.authorization
+  console.log(token)
   if(!token) return res.status(401).json({ auth: false, message: 'No token' })
   const decoded = jwt.verify(token, process.env.SECRET)
   req.userId = decoded.id
